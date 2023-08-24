d1bb54a54a3c837f6d24f61294ea03d9c3784742
Wil1999 <wil1999_@hotmail.com>
2023-08-23 23:05:52 -0500

    con fee 2

---
+++ info-kk/script.js
@@ -0,0 +1,27 @@
+var rc = document.getElementById('rotateChars')
+      var text = rc.innerText.trim()
+      var len = text.length
+      var grp = len / 2
+      var html = ""
+      while (len > -1) {
+          var txt = text.charAt(len)
+          html = `<span style="animation-delay:${len / grp}s" class="indChar ${txt.trim() === "" ? "" : "d-inline-block"} ">${txt}</span>${html}`
+          len--
+      }
+      rc.innerHTML = html
+      
+      var audioEnabled = false
+      var wik = document.getElementById('wik')
+      var navLinks = document.getElementsByClassName('nav-link')
+      for (var n of navLinks) {
+          n.addEventListener('mouseover', function () { if (audioEnabled) { wik.currentTime = 0; wik.play() } })
+      }
+      
+      var enableMusic = window.addEventListener('click', function () {
+          var myAudio = document.getElementById('myAudio')
+          myAudio.volume = 0.5;
+          myAudio.play()
+          audioEnabled = true
+          document.getElementById('mouse').style.animationPlayState = "paused"
+          removeEventListener('click', enableMusic)
+      })
